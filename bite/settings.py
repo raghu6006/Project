b38c08e862324176b63b841467bea5fc69696fad
Raghu <User.com>
2023-07-15 16:53:24 +0530

    Configured hosts

--- bite/settings.py
+++ bite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-_g!y8m#83t%z*01apbl5fyypqqtltq1mxywn(apadt5^fjg6$z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,11 @@
 echo "Waiting for the db service to become healthy..."
 
 python wait_for_db.py
+python manage.py makemigrations
 python manage.py migrate
 echo "The db service is now healthy."
 
 # Run the Django development server
+
 python manage.py runserver 0.0.0.0:80 
 #tail -f /dev/null
